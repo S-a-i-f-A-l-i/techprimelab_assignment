3abadbb9419db22e95c75265c6b47b4a3acfd4a2
S-a-i-f-A-l-i <saifali27906@gmail.com>
2023-06-01 15:57:08 +0530

    form added in login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
+import Login from "./pages/Login";
+
 function App() {
-  return <div className="App">App Component</div>;
+  return (
+    <div className="App">
+      <h1>App</h1>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
