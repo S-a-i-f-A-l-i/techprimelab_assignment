a2f552ba93d44544a51e9e1bd64334f67cd26311
S-a-i-f-A-l-i <saifali27906@gmail.com>
2023-06-09 12:46:26 +0530

    project listing done

--- client/src/pages/ProjectListing.js
+++ client/src/pages/ProjectListing.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { getProjectAction } from "../redux/actions/projectAction";
 import PaginationButton from "../component/PaginationButton";
+import styles from "../assets/styles/ProjectListing.module.css";
 
 const ProjectListing = () => {
   const timerId = useRef();
@@ -21,7 +22,7 @@ const ProjectListing = () => {
   const getProjects = async () => {
     try {
       const res = await axios.get(
-        `https://techprimelab-assignment-lmk9.onrender.com/project/all?search=${search}&page=${currentPage}`,
+        `https://techprimelab-assignment-lmk9.onrender.com/project/all?search=${search}&page=${currentPage}&limit=7`,
         {
           headers: { Authorization: `Bearer ${token}` },
         }
@@ -66,88 +67,175 @@ const ProjectListing = () => {
     getProjects();
   }, [projects, currentPage]);
   return (
-    <div>
-      <div>
-        <input
-          type="text"
-          name="search"
-          value={search}
-          onChange={optimizedDebounce}
-        />
-        <br />
+    <>
+      <div className={styles.Wrapper}>
+        <div>
+          <input
+            type="text"
+            name="search"
+            value={search}
+            onChange={optimizedDebounce}
+            placeholder="🔍 search"
+          />
+          <br />
+        </div>
+        {projects && (
+          <table>
+            <thead>
+              <tr>
+                <th>Project Name</th>
+                <th>Reason</th>
+                <th>Type</th>
+                <th>Division</th>
+                <th>Category</th>
+                <th>Priority</th>
+                <th>Department</th>
+                <th>Location</th>
+                <th>Status</th>
+                <th></th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {projects.projects.map((item, index) => {
+                return (
+                  <tr key={item._id}>
+                    <td>{item.name}</td>
+                    <td>{item.reason}</td>
+                    <td>{item.type}</td>
+                    <td>{item.division}</td>
+                    <td>{item.category}</td>
+                    <td>{item.priority}</td>
+                    <td>{item.department}</td>
+                    <td>{item.location}</td>
+                    <td>{item.status}</td>
+                    <td>
+                      <button onClick={() => handleClick(item._id, "running")}>
+                        Start
+                      </button>
+                    </td>
+                    <td>
+                      <button onClick={() => handleClick(item._id, "closed")}>
+                        Close
+                      </button>
+                    </td>
+                    <td>
+                      <button onClick={() => handleClick(item._id, "canceled")}>
+                        Cancel
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+        <div>
+          <button
+            disabled={currentPage <= 1}
+            onClick={() => setCurrentPage(currentPage - 1)}
+          >
+            Prev
+          </button>
+          {pages.map((item, index) => {
+            return (
+              <PaginationButton
+                key={index}
+                page={item}
+                handlePage={setCurrentPage}
+                // currentPage={currentPage}
+              />
+            );
+          })}
+          <button
+            disabled={currentPage >= projects.numOfPages}
+            onClick={() => setCurrentPage(currentPage + 1)}
+          >
+            Prev
+          </button>
+        </div>
       </div>
-      {projects && (
-        <table>
-          <thead>
-            <tr>
-              <th>Project Name</th>
-              <th>Reason</th>
-              <th>Type</th>
-              <th>Division</th>
-              <th>Category</th>
-              <th>Priority</th>
-              <th>Department</th>
-              <th>Location</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {projects.projects.map((item, index) => {
+      <div className={styles.container}>
+        <div>
+          <input
+            type="text"
+            name="search"
+            value={search}
+            onChange={optimizedDebounce}
+            placeholder="🔍 search"
+          />
+          <br />
+        </div>
+        <div>
+          {projects &&
+            projects.projects.map((item, index) => {
               return (
-                <tr key={item._id}>
-                  <td>{item.name}</td>
-                  <td>{item.reason}</td>
-                  <td>{item.type}</td>
-                  <td>{item.division}</td>
-                  <td>{item.category}</td>
-                  <td>{item.priority}</td>
-                  <td>{item.department}</td>
-                  <td>{item.location}</td>
-                  <td>{item.status}</td>
-                  <td>
+                <div key={item._id}>
+                  <div>
+                    <h3>{item.name}</h3>
+                    <h3>{item.status}</h3>
+                  </div>
+                  <div>
+                    <p>
+                      Reason: <span>{item.reason} </span>
+                    </p>
+                    <p>
+                      Type: <span>{item.type} </span>
+                      <span>O</span> Category<span>{item.category} </span>
+                    </p>
+                    <p>
+                      Div: <span>{item.division} </span>
+                      <span>O</span> Dept<span>{item.department} </span>
+                    </p>
+                    <p>
+                      Location: <span>{item.location} </span>
+                    </p>
+                    <p>
+                      Priority: <span>{item.priority} </span>
+                    </p>
+                  </div>
+                  <div>
                     <button onClick={() => handleClick(item._id, "running")}>
                       Start
                     </button>
-                  </td>
-                  <td>
                     <button onClick={() => handleClick(item._id, "closed")}>
                       Close
                     </button>
-                  </td>
-                  <td>
                     <button onClick={() => handleClick(item._id, "canceled")}>
                       Cancel
                     </button>
-                  </td>
-                </tr>
+                  </div>
+                </div>
               );
             })}
-          </tbody>
-        </table>
-      )}
-      <button
-        disabled={currentPage <= 1}
-        onClick={() => setCurrentPage(currentPage - 1)}
-      >
-        Prev
-      </button>
-      {pages.map((item, index) => {
-        return (
-          <PaginationButton
-            key={index}
-            page={item}
-            handlePage={setCurrentPage}
-            // currentPage={currentPage}
-          />
-        );
-      })}
-      <button
-        disabled={currentPage >= projects.numOfPages}
-        onClick={() => setCurrentPage(currentPage + 1)}
-      >
-        Prev
-      </button>
-    </div>
+        </div>
+        <div>
+          <button
+            disabled={currentPage <= 1}
+            onClick={() => setCurrentPage(currentPage - 1)}
+          >
+            Prev
+          </button>
+          {pages.map((item, index) => {
+            return (
+              <PaginationButton
+                key={index}
+                page={item}
+                handlePage={setCurrentPage}
+                // currentPage={currentPage}
+              />
+            );
+          })}
+          <button
+            disabled={currentPage >= projects.numOfPages}
+            onClick={() => setCurrentPage(currentPage + 1)}
+          >
+            Prev
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
